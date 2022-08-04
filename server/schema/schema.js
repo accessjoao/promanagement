4e55b6569cf54367f0fbfca282efa24ab116aff4
Joao Felipe Silveira <joaofe747@gmail.com>
2022-08-04 13:21:53 -0400

    working on schema

---
+++ server/schema/schema.js
@@ -0,0 +1,42 @@
+const { projects, clients } = require('../sampleData.js')
+
+const {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLEnumType,
+} = require('graphql');
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString },
+    }),
+});
+
+
+// Creating root query object to get client by ID
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        client: {
+            type: ClientType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return clients.find(client => client.id === args.id)
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+});
