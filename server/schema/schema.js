5d678d6cd34b69757af829765201ed5d57a66cc3
Joao Felipe Silveira <joaofe747@gmail.com>
2022-08-08 16:59:09 -0400

    Updating graphql schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,9 @@
-const { projects, clients } = require('../sampleData.js')
+// const { projects, clients } = require('../sampleData.js')
+
+//Importing mongoose models
+
+const Project = require('../models/Project')
+const Client = require('../models/Client')
 
 const {
     GraphQLObjectType,
@@ -22,7 +27,7 @@ const ProjectType = new GraphQLObjectType({
         client: {
             type: ClientType,
             resolve(parent, args) {
-                return clients.find((client) => client.id === parent.clientId);
+                return Client.findById(parent.clientId);
             }
         },
     }),
@@ -49,28 +54,30 @@ const RootQuery = new GraphQLObjectType({
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args) {
-                return projects;
+                return Project.find();
             }
         },
         project: {
             type: ProjectType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return projects.find(project => project.id === args.id)
+                return Project.findById(args.id)
             }
         },
         ////////////////////
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
-                return clients;
+                //return clients;
+                return Client.find();
             }
         },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return clients.find(client => client.id === args.id)
+                // return clients.find(client => client.id === args.id)
+                return Client.findById(args.id);
             }
         }
     }
