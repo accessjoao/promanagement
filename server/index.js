9fc2a51b6e6371ed09ebef4869932724f4c2b9a9
Joao Felipe Silveira <joaofe747@gmail.com>
2022-08-08 16:05:58 -0400

    connecting to database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const colors = require('colors')
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
+const connectDB = require('./conifg/db')
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Connecting to database
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
     schema,
