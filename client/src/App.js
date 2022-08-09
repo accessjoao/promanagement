ede89a2cb92933b035bd637a81679065e093489a
Joao Felipe Silveira <joaofe747@gmail.com>
2022-08-09 18:46:37 -0400

    Apollo set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <>
+    <ApolloProvider client={client}>
+    <Header />
     <div className="container">
       joao
     </div>
+    </ApolloProvider>
+    </>
   );
 }
 
