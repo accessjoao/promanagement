686b5d43bde39c172c0d319d4d42e8385fe3f61d
Joao Felipe Silveira <joaofe747@gmail.com>
2022-08-27 22:45:24 -0400

    working on projects

---
+++ client/src/components/Projects.jsx
@@ -0,0 +1,21 @@
+import Spinner from "./Spinner";
+import { useQuery } from "@apollo/client";
+import { GET_PROJECTS } from "../queries/projectQueries";
+
+export default function Projects() {
+
+  const { loading, error, data } = useQuery(GET_PROJECTS);
+
+  if (loading) return <Spinner />
+  if (error) return <p>Something Went Wrong</p>
+
+
+
+  return (
+  <>
+    {data.projects.length > 0 ? (
+      <div className="row">to be continued</div>
+    ) : (<p>No Projects</p>)}
+  </>
+  )
+}
