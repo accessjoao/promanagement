7741ad3a9a0c87f685c45f4a9ec0b4f22884a95c
Joao Felipe Silveira <joaofe747@gmail.com>
2022-09-02 16:19:02 -0400

    Update Project complete

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,17 +1,15 @@
-import {FaTrash} from 'react-icons/fa';
+
+import { FaTrash } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../mutations/clientMutations';
-import { GET_CLIENTS } from '../queries/clientQueries'
+import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 export default function ClientRow({ client }) {
-
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    refetchQueries: [{ query: GET_CLIENTS }]
-
-    // If I wanted to use cache to update UI
-
-     // update(cache, { data: { deleteClient } }) {
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
+    // update(cache, { data: { deleteClient } }) {
     //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
     //   cache.writeQuery({
     //     query: GET_CLIENTS,
@@ -24,15 +22,14 @@ export default function ClientRow({ client }) {
 
   return (
     <tr>
-      <td>{ client.name }</td>
-      <td>{ client.email }</td>
-      <td>{ client.phone }</td>
+      <td>{client.name}</td>
+      <td>{client.email}</td>
+      <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm"
-        onClick={deleteClient}>
+        <button className='btn btn-danger btn-sm' onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
     </tr>
-  )
+  );
 }
